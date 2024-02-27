da176824a4cd572c791e5d38fb68a43d4ff2ad06
romanbeaumont07 <romanbeaumont1999@gmail.com>
2024-02-26 18:16:55 -0600

    Style and Functionality Updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,50 @@ import { useState } from 'react'
 import './App.css'
 import Button from '@mui/material/Button';
 
+
+
+
 function App() {
+
   const [count, setCount] = useState(0)
+  if(count == 100){
+    window.alert("100 Click!!!")
+  }
   return (
+    
     <>
       <div>
       </div>
-      <h1>About Me!</h1>
+      <h1 className='header'>About Me!</h1>
       <p className="aboutMe">
        Hi! My name is Roman. I'm a 17 year old aspiring web developer!
       </p>
 
       <div className="card">
-        <Button variant='contained' onClick={() => setCount((count) => count + 1)}>
+        <Button
+        style={{backgroundColor:'#F64C72'}}
+        onClick={() => setCount((count) => count + 1)}
+        variant='contained'
+        elevation={60}
+        >
           count is {count}
         </Button> 
         <br />
         <br /> 
-        <Button variant='contained' href="https://github.com/romanbeaumont07">My Github</Button>
+        <Button 
+        style={{backgroundColor:'#F64C72'}}
+        variant='contained'
+        href="https://github.com/romanbeaumont07">
+          My Github
+        </Button>
         <br />
         <br />
-        <Button variant='contained' href="https://www.youtube.com/channel/UCEEjzQIRY2C40etJqP4njZQ">My Youtube</Button>
+        <Button 
+        style={{backgroundColor:'#F64C72'}}
+        variant='contained'
+        href="https://www.youtube.com/channel/UCEEjzQIRY2C40etJqP4njZQ">
+          My Youtube
+        </Button>
       </div>
       
     </>
